d321cbbddc9f05fa4a6850cf2fbd68905bd50a5f
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2023-09-02 07:56:21 +0000

    Init android

---
+++ .devcontainer/dependencies.sh
@@ -0,0 +1,61 @@
+#https://github.com/react-native-community/docker-android/blob/master/Dockerfile
+
+# SDK_VERSION=commandlinetools-linux-9477386_latest.zip
+# ANDROID_BUILD_VERSION=34
+# ANDROID_TOOLS_VERSION=34.0.0
+# NDK_VERSION=25.1.8937393
+# CMAKE_VERSION=3.22.1
+
+# ADB_INSTALL_TIMEOUT=10
+# ANDROID_HOME=/opt/android
+# ANDROID_SDK_ROOT=${ANDROID_HOME}
+# ANDROID_NDK_HOME=${ANDROID_HOME}/ndk/$NDK_VERSION
+
+# JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# CMAKE_BIN_PATH=${ANDROID_HOME}/cmake/$CMAKE_VERSION/bin
+
+# PATH=${CMAKE_BIN_PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/emulator:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${PATH}
+
+sudo apt update -qq && sudo apt install -qq -y --no-install-recommends \
+        apt-transport-https \
+        curl \
+        file \
+        gcc \
+        git \
+        g++ \
+        gnupg2 \
+        libgl1 \
+        libtcmalloc-minimal4 \
+        make \
+        openjdk-17-jdk-headless \
+        openssh-client \
+        patch \
+        python3 \
+        python3-distutils \
+        rsync \
+        ruby \
+        ruby-dev \
+        tzdata \
+        unzip \
+        sudo \
+        ninja-build \
+        zip \
+        libicu-dev \
+        jq \
+        shellcheck \
+    && sudo gem install bundler \
+    && sudo rm -rf /var/lib/apt/lists/*;
+
+sudo curl -sS https://dl.google.com/android/repository/${SDK_VERSION} -o /tmp/sdk.zip \
+&& sudo mkdir -p ${ANDROID_HOME}/cmdline-tools \
+&& sudo unzip -q -d ${ANDROID_HOME}/cmdline-tools /tmp/sdk.zip \
+&& sudo mv ${ANDROID_HOME}/cmdline-tools/cmdline-tools ${ANDROID_HOME}/cmdline-tools/latest \
+&& sudo rm /tmp/sdk.zip \
+&& yes | sudo ${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager --licenses \
+&& yes | sudo ${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager "platform-tools" \
+    "platforms;android-$ANDROID_BUILD_VERSION" \
+    "build-tools;$ANDROID_TOOLS_VERSION" \
+    "cmake;$CMAKE_VERSION" \
+    "ndk;$NDK_VERSION" \
+&& sudo rm -rf ${ANDROID_HOME}/.android \
+&& sudo chmod 777 -R /opt/android
---
+++ src/native/README.md
@@ -0,0 +1,44 @@
+# Android client
+
+## Debug device
+
+If pairing is needed, open developer tools on device:
+- turn on Wireless debugging
+- turn on pairing -> should provide pairing code + IP:PORT
+- type `adb pair IP:PORT`
+
+Connect to device with `adb connect IP:PORT`, note that port may have changed since pairing
+
+## Generate signing keys
+
+```
+keytool -genkeypair -v -storetype PKCS12 -keystore my-upload-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
+```
+
+## On every dev container recreation
+
+Create file `~/.gradle/gradle.properties` with contents:
+
+```
+MYAPP_UPLOAD_STORE_FILE=my-upload-key.keystore
+MYAPP_UPLOAD_KEY_ALIAS=my-key-alias
+MYAPP_UPLOAD_STORE_PASSWORD=*****
+MYAPP_UPLOAD_KEY_PASSWORD=*****
+```
+
+## Build a release
+```
+cd src/native/
+# To generate all the Android and IOS files
+npx expo prebuild --platform android
+
+# Make sure to connect your device or to run your Emulator
+# This generates APK
+npx react-native run-android --mode="release"
+
+# If you want to sign the APK and publish to Google Play Store.
+# This generates AAB
+npx react-native build-android --mode=release
+```
+
+Read [documentation](https://reactnative.dev/docs/signed-apk-android)
