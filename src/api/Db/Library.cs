f5287647136d57bacc2f0f7e3864c1cca6605ff9
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2023-09-15 14:53:58 +0000

    Simplify tracking

--- src/api/Db/Library.cs
+++ src/api/Db/Library.cs
@@ -7,27 +7,16 @@ public class Library
         Name = null!;
         MediaPath = null!;
     }
-    internal Library(string name, string mediaPath, LibraryType type)
+    internal Library(string name, string mediaPath)
     {
         Name = name;
         MediaPath = mediaPath;
-        Type = type;
     }
     public int LibraryId { get; set; }
     public string Name { get; set; }
     public string MediaPath { get; set; }
     public DateTime? LastIndex { get; set; }
-    public LibraryType Type { get; set; }
 
     public virtual List<User> Users { get; set; } = new();
     public virtual List<Video> Videos { get; set; } = new();
-    public virtual List<Movie> Movies { get; set; } = new();
-    public virtual List<Serie> Series { get; set; } = new();
-}
-
-public enum LibraryType
-{
-    Unknown = 0,
-    Movies = 1,
-    Series = 2,
 }
--- src/api/Db/_AppDbContext.cs
+++ src/api/Db/_AppDbContext.cs
@@ -23,15 +23,10 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
     public DbSet<DataProtectionKey> DataProtectionKeys { get; set; } = null!;
     public DbSet<Library> Libraries => Set<Library>();
     public DbSet<Video> Videos => Set<Video>();
-    public DbSet<Movie> Movies => Set<Movie>();
-    public DbSet<Serie> Series => Set<Serie>();
-    public DbSet<SerieEpisode> SerieEpisodes => Set<SerieEpisode>();
     public DbSet<User> Users => Set<User>();
     public DbSet<UserDevice> UserDevices => Set<UserDevice>();
     public DbSet<Watcher> Watchers => Set<Watcher>();
     public DbSet<WatcherVideo> WatcherVideos => Set<WatcherVideo>();
-    public DbSet<WatcherMovie> WatcherMovies => Set<WatcherMovie>();
-    public DbSet<WatcherSerieEpisode> WatcherSerieEpisodes => Set<WatcherSerieEpisode>();
 
     protected void AdditionalConfiguration(DbContextOptionsBuilder options)
     {
@@ -51,32 +46,12 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
             e.HasKey(e => e.LibraryId);
             e.HasMany(e => e.Users).WithMany(e => e.Libraries);
             e.HasMany(e => e.Videos).WithOne(e => e.Library).HasForeignKey(e => e.LibraryId).OnDelete(DeleteBehavior.Cascade);
-            e.HasMany(e => e.Movies).WithOne(e => e.Library).HasForeignKey(e => e.LibraryId).OnDelete(DeleteBehavior.Cascade);
-            e.HasMany(e => e.Series).WithOne(e => e.Library).HasForeignKey(e => e.LibraryId).OnDelete(DeleteBehavior.Cascade);
         });
 
         builder.Entity<Video>(e =>
         {
             e.HasKey(e => e.VideoId);
             e.HasMany(e => e.Views).WithOne(e => e.Video).HasForeignKey(e => e.VideoId).OnDelete(DeleteBehavior.Cascade);
-        });
-
-        builder.Entity<Movie>(e =>
-        {
-            e.HasKey(e => e.MovieId);
-            e.HasMany(e => e.Views).WithOne(e => e.Movie).HasForeignKey(e => e.MovieId).OnDelete(DeleteBehavior.Cascade);
-        });
-
-        builder.Entity<Serie>(e =>
-        {
-            e.HasKey(e => e.SerieId);
-            e.HasMany(e => e.Episodes).WithOne(e => e.Serie).HasForeignKey(e => e.SerieId).OnDelete(DeleteBehavior.Cascade);
-        });
-
-        builder.Entity<SerieEpisode>(e =>
-        {
-            e.HasKey(e => e.SerieEpisodeId);
-            e.HasMany(e => e.Views).WithOne(e => e.SerieEpisode).HasForeignKey(e => e.SerieEpisodeId).OnDelete(DeleteBehavior.Cascade);
         });
 
         builder.Entity<User>(e =>
@@ -101,16 +76,6 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
         builder.Entity<WatcherVideo>(e =>
         {
             e.HasKey(e => new { e.WatcherId, e.VideoId });
-        });
-
-        builder.Entity<WatcherMovie>(e =>
-        {
-            e.HasKey(e => new { e.WatcherId, e.MovieId });
-        });
-
-        builder.Entity<WatcherSerieEpisode>(e =>
-        {
-            e.HasKey(e => new { e.WatcherId, e.SerieEpisodeId });
         });
 
         // SQLite conversions
@@ -155,9 +120,9 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
         Users.AddRange(devUser, usrUser);
         await SaveChangesAsync();
 
-        var otherLibrary = new Library("Fake other", "fake_other", LibraryType.Unknown);
-        var movieLibrary = new Library("Fake movies", "fake_movie", LibraryType.Movies);
-        var serieLibrary = new Library("Fake series", "fake_series", LibraryType.Series);
+        var otherLibrary = new Library("Fake other", "fake_other");
+        var movieLibrary = new Library("Fake movies", "fake_movie");
+        var serieLibrary = new Library("Fake series", "fake_series");
 
         Libraries.AddRange(otherLibrary, movieLibrary, serieLibrary);
         await SaveChangesAsync();
@@ -183,20 +148,8 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
         foreach (var suffix in suffixes)
         {
             otherLibrary.Videos.Add(new Video($"Fake video {suffix}", $"fake_video_{suffix}.mkv") { Duration = TimeSpan.FromMinutes(90) });
-            movieLibrary.Movies.Add(new Movie($"Fake movie {suffix}", $"fake_movie_{suffix}.mkv") { Duration = TimeSpan.FromMinutes(120), Year = 2021 });
-            var series = new Serie($"Fake series {suffix}", $"fake_series_{suffix}");
-            for (int s = 1; s <= 2; s++)
-                for (int e = 1; e <= 5; e++)
-                {
-                    var id = $"S{s.ToString().PadLeft(2, '0')}E{e.ToString().PadLeft(2, '0')}";
-                    series.Episodes.Add(new SerieEpisode($"Episode {id}", $"fake_episode_{id}.mkv")
-                    {
-                        Duration = TimeSpan.FromMinutes(45),
-                        Season = s,
-                        Episode = e,
-                    });
-                }
-            serieLibrary.Series.Add(series);
+            otherLibrary.Videos.Add(new Video($"Fake movie {suffix}", $"fake_movie_{suffix}.mkv") { Duration = TimeSpan.FromMinutes(120) });
+            otherLibrary.Videos.Add(new Video($"Fake series {suffix}", $"fake_series_{suffix}.mkv") { Duration = TimeSpan.FromMinutes(45) });
         }
         await SaveChangesAsync();
 
@@ -204,16 +157,6 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
         AddWatchedVideo(otherLibrary.Videos[1], uno, 33);
         AddWatchedVideo(otherLibrary.Videos[1], dos, 53);
         AddWatchedVideo(otherLibrary.Videos[2], dos, 94);
-
-        AddWatchedMovie(movieLibrary.Movies[0], uno, 97);
-        AddWatchedMovie(movieLibrary.Movies[1], uno, 44);
-        AddWatchedMovie(movieLibrary.Movies[1], dos, 68);
-        AddWatchedMovie(movieLibrary.Movies[2], dos, 95);
-
-        AddWatchedEpisode(serieLibrary.Series[0], uno, 1, 1, 5);
-        AddWatchedEpisode(serieLibrary.Series[1], uno, 1, 1, 3);
-        AddWatchedEpisode(serieLibrary.Series[1], dos, 1, 1, 5);
-        AddWatchedEpisode(serieLibrary.Series[2], dos, 1, 1, 4);
 
         await SaveChangesAsync();
 
@@ -226,30 +169,6 @@ public partial class AppDbContext : DbContext, IDataProtectionKeyContext
                 Percentage = percentage,
                 LastPosition = percentage / 100d * video.Duration!.Value
             });
-        }
-
-        void AddWatchedMovie(Movie movie, Watcher watcher, int percentage)
-        {
-            WatcherMovies.Add(new()
-            {
-                MovieId = movie.MovieId,
-                WatcherId = watcher.WatcherId,
-                Percentage = percentage,
-                LastPosition = percentage / 100d * movie.Duration!.Value
-            });
-        }
-
-        void AddWatchedEpisode(Serie serie, Watcher watcher, int season, int firstEpisode, int lastEpisode)
-        {
-            var episodes = serie.Episodes.Where(e => e.Season == season && e.Episode >= firstEpisode && e.Episode <= lastEpisode);
-            foreach (var episode in episodes)
-                WatcherSerieEpisodes.Add(new()
-                {
-                    SerieEpisodeId = episode.SerieEpisodeId,
-                    WatcherId = watcher.WatcherId,
-                    Percentage = 92,
-                    LastPosition = 92 / 100d * episode.Duration!.Value
-                });
         }
     }
 }
